0d1b52ed0b17fcb342abf6f92162a7413dfbe5cf
Raman Jeganathan <raman.jeg@gmail.com>
2023-06-06 15:54:48 +0200

    seeded 3 cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_132637) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_134321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_132637) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "country"
+    t.string "longitude"
+    t.string "latitude"
   end
 
   create_table "hotels", force: :cascade do |t|
