a03046d3b9d8eb5b6fcfdb683bdb92d9ea1a85a7
Brendan Dodd <brendananthonydodd@gmail.com>
2023-06-06 16:43:02 +0200

    get names

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+filepath = "/Users/BrendanDodd/code/brendodd/PerriPlan/db/hotels/ams_hotels.json"
+serialized_hotel = File.read(filepath)
+info = JSON.parse(serialized_hotel)
+
+info['properties'].each do |property|
+  puts property['name']
+end
