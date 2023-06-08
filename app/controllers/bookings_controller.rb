b107e3f49fc3a17ce25931ed32ccde3f5cd27d5a
Brendan Dodd <brendananthonydodd@gmail.com>
2023-06-08 12:39:11 +0200

    added new and create to booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,5 +5,20 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @hotel = Hotel.find(params[:hotel_id])
+    @booking.bookable = @hotel
+    @booking.user = current_user
+    if @booking.save
+      redirect_to hotel_path(@hotel)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :note)
   end
 end
