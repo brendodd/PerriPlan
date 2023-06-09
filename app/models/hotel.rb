6aa8e0f3d06982a7bbbfe6d9eb17ff7ac8a80cd3
Davide Cifali <davide.cifali@hotmail.it>
2023-06-09 11:18:25 +0200

    Added multiple images

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,5 +1,6 @@
 class Hotel < ApplicationRecord
   has_many :bookings, as: :bookable
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
+  serialize :image_url, JSON
 end
