b1f83061236ef5b392c4ea27d98742abcef1f252
Davide Cifali <davide.cifali@hotmail.it>
2023-06-09 13:07:04 +0200

    Amsterdam map

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,14 @@
 class CitiesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :show]
+  skip_before_action :authenticate_user!, only: [:show]
   def show
     @city = City.find(params[:id])
     @hotels = Hotel.all
+    @markers = @hotels.geocoded.map do |hotel|
+      {
+        lat: hotel.latitude,
+        lng: hotel.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {hotel: hotel})
+      }
+    end
   end
 end
