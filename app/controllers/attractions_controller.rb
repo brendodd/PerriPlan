3d370ae6187931b4cc9a228e6c3abb602b3bb5c5
Raman Jeganathan <raman.jeg@gmail.com>
2023-06-15 16:12:38 +0200

    fixed attraction map

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -7,17 +7,16 @@ class AttractionsController < ApplicationController
         lat: restaurant.latitude,
         lng: restaurant.longitude,
         type: 'Restaurant',
-        attr_marker_html: render_to_string(partial: "cities/food_marker")
+        food_marker_html: render_to_string(partial: "cities/food_marker")
       }
     end
-    @hotels = Hotel.near([@restaurant.latitude, @restaurant.longitude], 2)
+    @hotels = Hotel.near([@attraction.latitude, @attraction.longitude], 2)
     @markers << @hotels.map do |hotel|
       { lat: hotel.latitude, lng: hotel.longitude, type: 'Hotel',
         hotel_marker_html: render_to_string(partial: "cities/hotel_marker") }
     end
     @markers << { lat: @attraction.latitude, lng: @attraction.longitude, type: 'Attraction',
-                  food_marker_html: render_to_string(partial: "cities/att_marker") }
+                  attr_marker_html: render_to_string(partial: "cities/att_marker") }
     @markers.flatten!
-    @reviews = @restaurant.reviews
   end
 end
