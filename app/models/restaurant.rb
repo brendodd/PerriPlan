8c703839ecc88878f2fdec5e584d82445dbdc908
Brendan Dodd <brendananthonydodd@gmail.com>
2023-06-15 10:20:12 +0200

    added favorites icon and page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,5 @@ class Restaurant < ApplicationRecord
   has_one_attached :photo
   geocoded_by :location
   after_validation :geocode # ,if: :will_save_change_to_address?
+  acts_as_favoritable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'favorites/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :hotels do
     resources :id, only: [:show]
     resources :bookings, only: [:new, :create]
+    resources :favorites, only: [:create]
   end
   # Defines the root path route ("/")
   # root "articles#index"
@@ -15,8 +17,11 @@ Rails.application.routes.draw do
 
   resources :restaurants, only: [:show] do
     resources :bookings, only: [:new, :create]
+    resources :favorites, only: [:create]
   end
-  
+
   resources :bookings, only: [:show, :index]
-  resources :attractions, only: [:show]
+  resources :attractions, only: [:show] do
+    resources :favorites, only: [:create]
+  end
 end
