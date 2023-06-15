b192b4b010ae4f0e41386a1915d17f70acda3960
Raman Jeganathan <raman.jeg@gmail.com>
2023-06-15 15:41:48 +0200

    add icons

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -5,12 +5,19 @@ class AttractionsController < ApplicationController
     @markers = @restaurants.map do |restaurant|
       {
         lat: restaurant.latitude,
-        lng: restaurant.longitude
+        lng: restaurant.longitude,
+        type: 'Restaurant',
+        attr_marker_html: render_to_string(partial: "cities/food_marker")
       }
     end
-    @hotels = Hotel.near([@attraction.latitude, @attraction.longitude], 2)
-    @markers << @hotels.map { |hotel| { lat: hotel.latitude, lng: hotel.longitude }}
-    @markers << { lat: @attraction.latitude, lng: @attraction.longitude }
+    @hotels = Hotel.near([@restaurant.latitude, @restaurant.longitude], 2)
+    @markers << @hotels.map do |hotel|
+      { lat: hotel.latitude, lng: hotel.longitude, type: 'Hotel',
+        hotel_marker_html: render_to_string(partial: "cities/hotel_marker") }
+    end
+    @markers << { lat: @attraction.latitude, lng: @attraction.longitude, type: 'Attraction',
+                  food_marker_html: render_to_string(partial: "cities/att_marker") }
     @markers.flatten!
+    @reviews = @restaurant.reviews
   end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,12 +16,18 @@ class RestaurantsController < ApplicationController
     @markers = @attractions.map do |attraction|
       {
         lat: attraction.latitude,
-        lng: attraction.longitude
+        lng: attraction.longitude,
+        type: 'Attraction',
+        attr_marker_html: render_to_string(partial: "cities/att_marker")
       }
     end
     @hotels = Hotel.near([@restaurant.latitude, @restaurant.longitude], 2)
-    @markers << @hotels.map { |hotel| { lat: hotel.latitude, lng: hotel.longitude } }
-    @markers << { lat: @restaurant.latitude, lng: @restaurant.longitude }
+    @markers << @hotels.map do |hotel|
+      { lat: hotel.latitude, lng: hotel.longitude, type: 'Hotel',
+        hotel_marker_html: render_to_string(partial: "cities/hotel_marker") }
+    end
+    @markers << { lat: @restaurant.latitude, lng: @restaurant.longitude, type: 'Restaurant',
+                  food_marker_html: render_to_string(partial: "cities/food_marker") }
     @markers.flatten!
     @reviews = @restaurant.reviews
   end
